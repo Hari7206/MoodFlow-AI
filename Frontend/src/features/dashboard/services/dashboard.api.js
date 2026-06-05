a23e411b8ddaa291257795dc5bcd8230ec1b353e
Hari Thapa <harithapa4654@gmail.com>
2026-06-06 02:23:57 +0530

    latest

--- Frontend/src/features/dashboard/services/dashboard.api.js
+++ Frontend/src/features/dashboard/services/dashboard.api.js
@@ -1,15 +1,15 @@
 import axios from "axios"
 
-
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: `${BASE_URL}/api/`,
   withCredentials: true
 })
 
 
 export async function getDashboard() {
-  const res = await api.get("/dashboard")
+  const res = await api.get("dashboard")
   return res.data
 }
 
@@ -20,15 +20,12 @@ export async function updateUser(file, data = {}) {
 
     if (file) {
       const formData = new FormData()
-
       formData.append("profilePic", file)
-
       if (data.name) formData.append("name", data.name)
       if (data.bio) formData.append("bio", data.bio)
-
-      response = await api.patch("/users/update", formData)
+      response = await api.patch("users/update", formData)
     } else {
-      response = await api.patch("/users/update", data)
+      response = await api.patch("users/update", data)
     }
 
     return response.data
