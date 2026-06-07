358253e10825e3bf20d7264d327edd639828a647
Hari Thapa <harithapa4654@gmail.com>
2026-06-07 11:40:38 +0530

    loading page

--- Frontend/src/features/auth/components/Procteted.jsx
+++ Frontend/src/features/auth/components/Procteted.jsx
@@ -1,15 +1,65 @@
 import { useAuth } from '../hooks/useAuth'
 import { Navigate } from 'react-router-dom'
+import "./Pocteted.css"
 
 function Procteted({ children }) {
 
     const { user, loading } = useAuth()
 
 
-    if (loading) {
-        return <h1>Loading</h1>
-    }
+    if (loading) return (
+  <div className="db__loading">
 
+    {/* Spotlights */}
+    <div className="db__spotlight db__spotlight--red" />
+    <div className="db__spotlight db__spotlight--dark" />
+    <div className="db__spotlight db__spotlight--red2" />
+
+    {/* Background Floating Boxes */}
+    <div className="db__bgbox db__bgbox--1" />
+    <div className="db__bgbox db__bgbox--2" />
+    <div className="db__bgbox db__bgbox--3" />
+    <div className="db__bgbox db__bgbox--4" />
+    <div className="db__bgbox db__bgbox--5" />
+    <div className="db__bgbox db__bgbox--6" />
+
+    {/* Center */}
+    <div className="db__center">
+
+      {/* Vinyl */}
+      <div className="db__vinyl-wrap">
+        <div className="db__vinyl" />
+        <div className="db__vinyl-label" />
+        <div className="db__vinyl-hole" />
+      </div>
+
+      {/* EQ Bars */}
+      <div className="db__eq">
+        <div className="db__eq-bar db__eq-bar--1" />
+        <div className="db__eq-bar db__eq-bar--2" />
+        <div className="db__eq-bar db__eq-bar--3" />
+        <div className="db__eq-bar db__eq-bar--4" />
+        <div className="db__eq-bar db__eq-bar--5" />
+        <div className="db__eq-bar db__eq-bar--6" />
+        <div className="db__eq-bar db__eq-bar--7" />
+      </div>
+
+      {/* Loading Boxes Row */}
+      <div className="db__boxes-row">
+        <div className="db__lbox db__lbox--1" />
+        <div className="db__lbox db__lbox--2" />
+        <div className="db__lbox db__lbox--3" />
+        <div className="db__lbox db__lbox--4" />
+        <div className="db__lbox db__lbox--5" />
+      </div>
+
+      {/* Text */}
+      <div className="db__label">Loading…</div>
+
+    </div>
+  </div>
+);
+ 
     if (!user) {
         return <Navigate to="/login" />
     }
